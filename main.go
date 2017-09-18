ccd5aab42a44dfd104ee6ebe7ab9b99772356b8c
Dusty Wilson <dusty@linux.com>
2017-09-18 06:37:32 +0000

    Added experimental -colormod

--- main.go
+++ main.go
@@ -13,11 +13,16 @@ func main() {
 	zip := flag.String("zip", "", "ZIP Code")
 	radius := flag.Int("radius", 25, "Radius")
 	variable := flag.String("var", "aqi", "Return: {aqi,color,category}")
+	colormod := flag.Int("colormod", 1, "I'm not sure about this yet")
 	flag.Parse()
 
 	if key == nil || *key == "" || zip == nil || *zip == "" || radius == nil || *radius < 1 {
 		flag.Usage()
 		os.Exit(1)
+	}
+
+	if colormod == nil || *colormod <= 0 {
+		*colormod = 1
 	}
 
 	an := airnow.New(*key)
@@ -31,7 +36,7 @@ func main() {
 	case "aqi":
 		fmt.Println(ob.AQI)
 	case "color":
-		fmt.Printf("%d,%d,%d\n", ob.Category.Color.R, ob.Category.Color.G, ob.Category.Color.B)
+		fmt.Printf("%d,%d,%d\n", ob.Category.Color.R/uint8(*colormod), ob.Category.Color.G/uint8(*colormod), ob.Category.Color.B/uint8(*colormod))
 	case "category":
 		fmt.Println(ob.Category.Num)
 	default:
